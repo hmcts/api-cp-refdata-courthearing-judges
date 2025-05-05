d797063bfe8838c1f5176732719222bf14617213
Duncan Crawford <duncan@noughtsandones.co.uk>
2025-05-05 19:22:52 +0100

    fix static code anaylysis failures

--- src/main/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoader.java
+++ src/main/java/uk/gov/hmcts/cp/config/OpenAPIConfigurationLoader.java
@@ -2,8 +2,6 @@ package uk.gov.hmcts.cp.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.OpenAPIV3Parser;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,7 +11,7 @@ import java.nio.charset.StandardCharsets;
 
 public class OpenAPIConfigurationLoader {
 
-    private static String JUDGES_OPENAPI_YML = "openapi/judges.openapi.yml";
+    private static final String JUDGES_OPENAPI_YML = "openapi/judges.openapi.yml";
 
     public OpenAPI openAPI() {
         return loadOpenApiFromClasspath(JUDGES_OPENAPI_YML);
@@ -21,7 +19,9 @@ public class OpenAPIConfigurationLoader {
 
     public static OpenAPI loadOpenApiFromClasspath(String path) {
         try (InputStream inputStream = OpenAPIConfigurationLoader.class.getClassLoader().getResourceAsStream(path)) {
-            if (inputStream == null) throw new IllegalArgumentException("Missing resource: " + path);
+            if (inputStream == null) {
+                throw new IllegalArgumentException("Missing resource: " + path);
+            }
             String yaml = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
             return new OpenAPIV3Parser().readContents(yaml, null, null).getOpenAPI();
         } catch (IOException e) {
